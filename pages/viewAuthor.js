de720af54076cbf3b74e9fcfa8fd2e18ebed64de
Johnny Saniat <Jcsaniat@gmail.com>
2023-08-23 17:13:56 -0500

    syled the cards and augmented layout

--- pages/viewAuthor.js
+++ pages/viewAuthor.js
@@ -8,14 +8,15 @@ const viewAuthor = (obj) => {
   domString += `
   <div class="mt-5 d-flex flex-wrap">
    <div class="d-flex flex-column">
-     <div class="mt-5">
-       <i id="edit-author-btn--${obj.firebaseKey}" class="fas fa-edit btn btn-info"></i>
-       <i id="delete-author--${obj.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
-     </div>
    </div>
    <div class="text-white ms-5 details">
      <h5>${obj.first_name} ${obj.last_name} ${obj.favorite ? '<span class="badge bg-danger"><i class="fa fa-heart" aria-hidden="true"></i></span>' : ''}</h5>
      Author Email: <a href="mailto:${obj.email}">${obj.email}</a>
+     <div class="mt-5">
+     <i id="edit-author-btn--${obj.firebaseKey}" class=" btn btn-info">EDIT AUTHOR</i>
+     <i id="delete-author--${obj.firebaseKey}" class="btn btn-danger">DELETE AUTHOR</i>
+   </div>
+   <hr>
       </div>
     </div>`;
 
@@ -23,12 +24,13 @@ const viewAuthor = (obj) => {
     domString += `
           <div class="card">
             <div class="card-body">
+            <img src=${item.image} alt=${item.title} style="width: 150px;">
               <h5 class="card-title">${item.title}</h5>
                 <p class="card-text bold">${item.sale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${item.price}` : `$${item.price}`}</p>
                 <hr>
-                <i class="btn btn-success fas fa-eye" id="view-book-btn--${item.firebaseKey}"></i>
-                <i id="edit-book-btn--${item.firebaseKey}" class="fas fa-edit btn btn-info"></i>
-                <i id="delete-book-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
+                <i class="btn btn-success" id="view-book-btn--${item.firebaseKey}">VIEW</i>
+                <i id="edit-book-btn--${item.firebaseKey}" class="btn btn-info">EDIT</i>
+                <i id="delete-book-btn--${item.firebaseKey}" class="btn btn-danger">Delete</i>
             </div>
           </div>`;
   });
